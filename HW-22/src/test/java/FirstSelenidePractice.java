8aa73c7bb362218ade8b6f54428f57c90f2e00c2
AleksandrQA21 <aleksandr.titovskoy@gmail.com>
2022-06-08 15:35:32 +0300

    Added JUnit

--- HW-22/src/test/java/FirstSelenidePractice.java
+++ HW-22/src/test/java/FirstSelenidePractice.java
@@ -1,25 +1,34 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
-import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.aesh.readline.terminal.Key.d;
 
 public class FirstSelenidePractice {
+
+    @BeforeEach
+    public void setUp(){
+        Configuration.browserSize = "1920x1080";
+        //Иногда страница долго грузится, поэтому вставил таймаут, чтобы тесты не падали.
+        open("http://automationpractice.com/index.php");
+    }
+
+    @RepeatedTest(3)
     @Test
     public void isLogoDisplayed(){
-        Configuration.browserSize = "1920x1080";
-        open("http://automationpractice.com/index.php");
 
         $(By.xpath("//img[@class='logo img-responsive']")).shouldBe(visible);
     }
 
     @Test
     public void isSearchFieldDisplayed(){
-        open("http://automationpractice.com/index.php");
-        //Иногда страница долго грузится, поэтому вставил таймаут, чтобы тесты не падали.
-        Configuration.timeout = 10000;
 
         $(By.xpath("//input[@class='search_query form-control ac_input']")).shouldBe(visible);
         $(By.xpath("//input[@class='search_query form-control ac_input']")).shouldBe(empty);
@@ -27,8 +36,6 @@ public class FirstSelenidePractice {
 
     @Test
     public void checkSearchFieldFunctionality(){
-        Configuration.browserSize = "1920x1080";
-        open("http://automationpractice.com/index.php");
 
         $(By.xpath("//input[@class='search_query form-control ac_input']")).shouldBe(empty);
         $(By.xpath("//input[@class='search_query form-control ac_input']")).setValue("Blouse");
@@ -40,8 +47,6 @@ public class FirstSelenidePractice {
 
     @Test
     public void checkCreateAccountPage(){
-        Configuration.browserSize = "1920x1080";
-        open("http://automationpractice.com/index.php");
 
         $(By.xpath("//a[@class='login']")).should(visible);
         $(By.xpath("//a[@class='login']")).click();
@@ -54,24 +59,22 @@ public class FirstSelenidePractice {
         $(By.xpath("//div[@id='columns']")).shouldBe(visible);
     }
 
-    @Test
-    public void checkInputInvalidEmail(){
-        Configuration.browserSize = "1920x1080";
-        open("http://automationpractice.com/index.php");
+    @ParameterizedTest
+    @ValueSource(strings = {"testgmail.com", "test@@gmail.com"})
+    public void checkInputInvalidEmail(String val){
 
         $(By.xpath("//a[@class='login']")).should(visible);
         $(By.xpath("//a[@class='login']")).click();
         $(By.xpath("//h3[contains(text(), 'Create an account')]")).shouldBe(visible);
-        $(By.xpath("//input[@id='email_create']")).setValue("testgmail.com");
+        $(By.xpath("//input[@id='email_create']")).setValue(val);
         $(By.xpath("//i[@class='icon-user left']")).click();
         Configuration.timeout = 10000;
         $(By.xpath("//div[@id='create_account_error']/ol/li")).shouldHave(exactText("Invalid email address."));
     }
 
+
     @Test
     public void checkDaysDropDown31Days(){
-        Configuration.browserSize = "1920x1080";
-        open("http://automationpractice.com/index.php");
 
         $(By.xpath("//a[@class='login']")).should(visible);
         $(By.xpath("//a[@class='login']")).click();
